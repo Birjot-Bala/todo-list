3d54bbd72194e3498b1812b6eac46e85596582f8
Birjot Bala <birjot.bala@gmail.com>
2021-01-24 17:56:22 -0500

    Missed new on class instance creation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Controller } from "./controller";
 import { Model } from "./model";
-import "./style.css";
 import { View } from "./view";
+import "./style.css";
 
-const app = Controller(new Model(), new View(document));
+const app = new Controller(new Model(), new View(document));
